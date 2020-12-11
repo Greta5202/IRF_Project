79a8ec4a025380110c11fcb8a422a9f1256e210c
Gréta Nováky <greta.novaky@stud.uni-corvinus.hu>
2020-12-11 15:11:11 +0100

    vaszon osztaly letrehozasa

---
+++ UC2PYY_irf_beadando/vaszon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC2PYY_irf_beadando
+{
+    class vaszon : Button
+    {
+        public vaszon()
+        {
+            Height = 20;
+            Width = 210;
+            BackColor = Color.Gray;
+            Text = "Vászon";
+            Enabled = false;
+
+        }
+    }
+}
